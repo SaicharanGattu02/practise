734000db8ff38d557db9f6e2e9732a78cc6be2cc
Saicharan02 <Charangattu643@gmail.com>
2024-12-03 17:21:16 +0530

    committed by charan 03-12-2024 at 5:21 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 /// [Widget] building the [MaterialApp].
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,7 +28,7 @@ class MyApp extends StatelessWidget {
               Icons.photo,
             ],
             builder: (item, isDragging){
-              return AnimatedContainer(
+              return AnimatedContainer (
                 duration:  Duration(milliseconds: 300),
                 curve: Curves.easeInOut,
                 constraints:  BoxConstraints(minWidth: 48),
@@ -31,9 +36,8 @@ class MyApp extends StatelessWidget {
                 margin:  EdgeInsets.all(8),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
-                  color: isDragging
-                      ? Colors.grey.shade300
-                      : Colors.primaries[item.hashCode % Colors.primaries.length],
+                  color: Colors.primaries[item.hashCode % Colors.primaries.length
+                  ],
                 ),
                 child: Icon(
                   item,
@@ -77,6 +81,9 @@ class _DockState<T extends Object> extends State<Dock<T>> with SingleTickerProvi
   /// Currently dragged item.
   T? _draggedItem;
 
+  bool isHovered=false;
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -85,70 +92,91 @@ class _DockState<T extends Object> extends State<Dock<T>> with SingleTickerProvi
         color: Colors.black12,
       ),
       padding:  EdgeInsets.all(4),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          double itemWidth = 60.0;
-
-          return Stack(
-            alignment: Alignment.center,
-            children: _items
-                .asMap()
-                .entries
-                .map((entry) {
-              final index = entry.key;
-              final item = entry.value;
-
-              return AnimatedPositioned(
-                key: ValueKey(item),
-                duration: Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-                left: index * itemWidth,
-                child: Draggable<T>(
-                  data: item,
-                  feedback: Material(
-                    color: Colors.transparent,
-                    child: widget.builder(item, true),
-                  ),
-                  childWhenDragging: const SizedBox.shrink(),
-                  onDragStarted: () {
-                    setState(() {
-                      _draggingIndex = index;
-                      _draggedItem = item;
-                    });
-                  },
-                  onDragEnd: (details) {
-                    setState(() {
-                      _draggingIndex = null;
-                      _draggedItem = null;
-                    });
-                  },
-                  child: DragTarget<T>(
-                    onWillAccept: (incomingItem) {
-                      if (incomingItem != item) {
-                        final oldIndex = _items.indexOf(incomingItem!);
-                        setState(() {
-                          _items.removeAt(oldIndex);
-                          _items.insert(index, incomingItem);
-                        });
-                      }
-                      return true;
-                    },
-                    onAccept: (_) {
-                      setState(() {
-                        _draggingIndex = null;
-                        _draggedItem = null;
-                      });
-                    },
-                    builder: (context, candidateData, rejectedData) {
-                      return widget.builder(item, _draggingIndex == index);
-                    },
-                  ),
-                ),
-              );
-            })
-                .toList(),
-          );
-        },
+      child: MouseRegion(
+        onEnter: (event) => setState(() {
+          isHovered=true;
+        }),
+        onHover: (event) => setState(() {
+          isHovered=true;
+        }),
+        onExit: (event) => setState(() {
+          isHovered=false;
+        }),
+        child: Center(
+          child: Container(
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.white,
+            ),
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                double itemWidth = 60.0;
+                return Stack(
+                  alignment: Alignment.centerLeft,
+                  children: _items
+                      .asMap()
+                      .entries
+                      .map((entry) {
+                    final index = entry.key;
+                    final item = entry.value;
+                    return AnimatedPositioned(
+                      key: ValueKey(item),
+                      duration: Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      left: index * itemWidth,
+                      child: Transform.scale(
+                        scale: isHovered && index==entry.key ? 1.2:1.0,
+                        child: Draggable<T>(
+                          data: item,
+                          feedback: Material(
+                            color: Colors.transparent,
+                            child: widget.builder(item, true),
+                          ),
+                          childWhenDragging: const SizedBox.shrink(),
+                          onDragStarted: () {
+                            setState(() {
+                              _draggingIndex = index;
+                              _draggedItem = item;
+                            });
+                          },
+                          onDragEnd: (details) {
+                            setState(() {
+                              _draggingIndex = null;
+                              _draggedItem = null;
+                            });
+                          },
+                          child: DragTarget<T>(
+                            onWillAccept: (incomingItem) {
+                              if (incomingItem != item) {
+                                final oldIndex = _items.indexOf(incomingItem!);
+                                setState(() {
+                                  _items.removeAt(oldIndex);
+                                  _items.insert(index, incomingItem);
+                                });
+                              }
+                              return true;
+                            },
+                            onAccept: (_) {
+                              setState(() {
+                                _draggingIndex = null;
+                                _draggedItem = null;
+                              });
+                            },
+                            builder: (context, candidateData, rejectedData) {
+                              return widget.builder(item, _draggingIndex == index);
+                            },
+                          ),
+                        ),
+                      ),
+                    );
+                  })
+                      .toList(),
+                );
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
